03247030a160ff9ed18b17838a5bfa93c93cf990
Gustavo Arantes <me@arantes.dev>
2023-07-09 12:34:17 -0300

    ⚡️ Use connection pools to allow concurrency

--- database/connection.go
+++ database/connection.go
@@ -5,15 +5,15 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func GetConnection(uri string) *pgx.Conn {
-	conn, err := pgx.Connect(context.Background(), uri)
+func CreatePool(uri string) *pgxpool.Pool {
+	pool, err := pgxpool.New(context.Background(), uri)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
 
-	return conn
+	return pool
 }
